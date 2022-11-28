80abea36abd4609c94ea4a30f62d0237a7e90de8
Hyndavi0713 <118278386+Hyndavi0713@users.noreply.github.com>
2022-11-28 17:49:54 +0000

    RETRIEVE

---
+++ Retrieve.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADO.NET
+{
+    internal class Retrieve
+    {
+        public static void Main(string[] args)
+        {
+            string connectionString = " data source =.;Database=honeyVM; integrated security = 'True'";
+
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                con.Open();
+                using (SqlCommand cmd = new SqlCommand())
+                {
+
+                    cmd.Connection = con;
+                    cmd.CommandText = "SELECT * FROM student";
+
+
+                    SqlDataReader dr = cmd.ExecuteReader();
+
+                    while (dr.Read())
+                    {
+                        string stid = dr["stid"].ToString();
+                        string stname = dr["stname"].ToString();
+                        string stgroup = dr["stgroup"].ToString();
+
+                        Console.WriteLine(stid + "  " + stname + "  " + stgroup);
+                    }
+                    dr.Close();
+
+                }
+            }
+            Console.ReadKey();
+            Console.ReadLine();
+        }
+    }
+}
